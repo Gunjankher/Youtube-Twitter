e5a71a22cea87920c9ce4540acfa858062f04367
Gunjankher <gkhercode@gmail.com>
2024-10-23 10:35:48 +0530

    comment controller

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userSliceReducer from './Slices/userSlice.js'
+import authSliceReducer from './Slices/authSlice.js'
+import commentSliceReducer from './Slices/commentSlice.js'
 
 const store = configureStore({
 reducer :{
     user:userSliceReducer,
+    auth:authSliceReducer,
+    comment:commentSliceReducer,
 }
 })
 
