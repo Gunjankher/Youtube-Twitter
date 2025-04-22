422a17108227cbcd1958fa4a2815676e550bcffd
Gunjankher <gkhercode@gmail.com>
2025-04-22 17:43:56 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -38,7 +38,7 @@ import likeRouter from './routes/like.router.js'
 import commentRouter from'./routes/comment.router.js'
 import tweetRouter from './routes/tweet.router.js'
 import playlistRouter from './routes/playlist.router.js'
-import healthRouter from './routes/healthcheck.router.js' 
+// import healthRouter from './routes/healthcheck.router.js' 
 import subscripitonRouter from './routes/subscription.router.js'
 import dashboardRouter from './routes/dashboard.router.js'
 
@@ -50,7 +50,7 @@ app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/playlist",playlistRouter)
-app.use("/api/v1/healthcheck",healthRouter)
+// app.use("/api/v1/healthcheck",healthRouter)
 app.use("/api/v1/subscriptions",subscripitonRouter)
 app.use("/api/v1/dashboard",dashboardRouter)
 
