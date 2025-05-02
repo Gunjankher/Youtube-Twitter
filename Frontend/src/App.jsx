d7532a160f61ee1656844c0da96ec6e3555d1d10
Gunjankher <gkhercode@gmail.com>
2025-05-02 22:49:10 +0530

    adding lazy loading in routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,218 +1,213 @@
-import { useState } from 'react'
-import React, { useEffect } from "react";
+import { useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { getCurrentUser } from "./store/Slices/authSlice";
-import Layout from './Layout';
-import HomePage from './pages/HomePage';
-import AuthLayout from './components/AuthLayout'
-import Login from './components/Login';
-import SignUp from './components/Signup';
-import TermsAndConditions from './pages/TermsAndConditions'
-import LikedVideos from './pages/LikedVidoes';
-import History from './pages/History';
-import Channel from './pages/Channel/Channel';
-import ChannelVideos from './pages/Channel/ChannelVideos';
-import ChannelSubscribers from './pages/Channel/ChannelSubscribers'
-import ChannelPlaylist from './pages/Channel/ChannelPlaylist';
-import ChannelTweets from './pages/Channel/ChannelTweets';
-import AdminDashboard from './pages/AdminDashboard';
-import EditChannel from './pages/EditChannel';
-import EditPersonalInfo from './components/EditPersonalInfo';
-import ChangePassword from './components/ChangePassword';
-import VideoDetail from './pages/VideoDetail';
-import MySubscriptions from './pages/MySubscription'
-import SearchVideos from './pages/SearchVideos';
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader"; // Simple loading spinner
 
-
-
-
-
-
-
-
+// Lazy-loaded route components
+const Layout = lazy(() => import('./Layout'));
+const AuthLayout = lazy(() => import('./components/AuthLayout'));
+const HomePage = lazy(() => import('./pages/HomePage'));
+const Login = lazy(() => import('./components/Login'));
+const SignUp = lazy(() => import('./components/Signup'));
+const TermsAndConditions = lazy(() => import('./pages/TermsAndConditions'));
+const LikedVideos = lazy(() => import('./pages/LikedVidoes'));
+const History = lazy(() => import('./pages/History'));
+const Channel = lazy(() => import('./pages/Channel/Channel'));
+const ChannelVideos = lazy(() => import('./pages/Channel/ChannelVideos'));
+const ChannelSubscribers = lazy(() => import('./pages/Channel/ChannelSubscribers'));
+const ChannelPlaylist = lazy(() => import('./pages/Channel/ChannelPlaylist'));
+const ChannelTweets = lazy(() => import('./pages/Channel/ChannelTweets'));
+const AdminDashboard = lazy(() => import('./pages/AdminDashboard'));
+const EditChannel = lazy(() => import('./pages/EditChannel'));
+const EditPersonalInfo = lazy(() => import('./components/EditPersonalInfo'));
+const ChangePassword = lazy(() => import('./components/ChangePassword'));
+const VideoDetail = lazy(() => import('./pages/VideoDetail'));
+const MySubscriptions = lazy(() => import('./pages/MySubscription'));
+const SearchVideos = lazy(() => import('./pages/SearchVideos'));
 
 function App() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getCurrentUser());
-    }, [dispatch]);
-
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
 
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Layout />}>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
             {/* Public Routes */}
             <Route
-                index
-                element={
-                    <AuthLayout authentication={false}>
-                        <HomePage />
-                    </AuthLayout>
-                }
+              index
+              element={
+                <AuthLayout authentication={false}>
+                  <HomePage />
+                </AuthLayout>
+              }
             />
             <Route
-                path="login"
-                element={
-                    <AuthLayout authentication={false}>
-                        <Login />
-                    </AuthLayout>
-                }
+              path="login"
+              element={
+                <AuthLayout authentication={false}>
+                  <Login />
+                </AuthLayout>
+              }
             />
             <Route
-                path="signup"
-                element={
-                    <AuthLayout authentication={false}>
-                        <SignUp />
-                    </AuthLayout>
-                }
+              path="signup"
+              element={
+                <AuthLayout authentication={false}>
+                  <SignUp />
+                </AuthLayout>
+              }
             />
             <Route
-                path="terms&conditions"
-                element={
-                    <AuthLayout authentication={false}>
-                        <TermsAndConditions />
-                    </AuthLayout>
-                }
+              path="terms&conditions"
+              element={
+                <AuthLayout authentication={false}>
+                  <TermsAndConditions />
+                </AuthLayout>
+              }
             />
             <Route
-                path="search/:query"
-                element={
-                    <AuthLayout authentication={false}>
-                        <SearchVideos />
-                    </AuthLayout>
-                }
+              path="search/:query"
+              element={
+                <AuthLayout authentication={false}>
+                  <SearchVideos />
+                </AuthLayout>
+              }
             />
 
             {/* Protected Routes */}
             <Route
-                path="history"
-                element={
-                    <AuthLayout authentication>
-                        <History />
-                    </AuthLayout>
-                }
+              path="history"
+              element={
+                <AuthLayout authentication>
+                  <History />
+                </AuthLayout>
+              }
             />
             <Route
-                path="liked-videos"
-                element={
-                    <AuthLayout authentication>
-                        <LikedVideos />
-                    </AuthLayout>
-                }
+              path="liked-videos"
+              element={
+                <AuthLayout authentication>
+                  <LikedVideos />
+                </AuthLayout>
+              }
             />
             <Route
-                path="subscriptions"
-                element={
-                    <AuthLayout authentication>
-                        <MySubscriptions />
-                    </AuthLayout>
-                }
+              path="subscriptions"
+              element={
+                <AuthLayout authentication>
+                  <MySubscriptions />
+                </AuthLayout>
+              }
             />
             <Route
-                path="collections"
-                element={
-                    <AuthLayout authentication>
-                        <AdminDashboard />
-                    </AuthLayout>
-                }
+              path="collections"
+              element={
+                <AuthLayout authentication>
+                  <AdminDashboard />
+                </AuthLayout>
+              }
             />
             <Route
-                path="watch/:videoId"
-                element={
-                    <AuthLayout authentication>
-                        <VideoDetail />
-                    </AuthLayout>
-                }
+              path="watch/:videoId"
+              element={
+                <AuthLayout authentication>
+                  <VideoDetail />
+                </AuthLayout>
+              }
             />
 
             {/* Channel Routes with Nested Routes */}
             <Route
-                path="channel/:username"
-                element={
-                    <AuthLayout authentication>
-                        <Channel />
-                    </AuthLayout>
-                }
+              path="channel/:username"
+              element={
+                <AuthLayout authentication>
+                  <Channel />
+                </AuthLayout>
+              }
             >
-                <Route
-                    path="videos"
-                    element={
-                        <AuthLayout authentication>
-                            <ChannelVideos />
-                        </AuthLayout>
-                    }
-                />
-                <Route
-                    path="playlists"
-                    element={
-                        <AuthLayout authentication>
-                            <ChannelPlaylist />
-                        </AuthLayout>
-                    }
-                />
-                <Route
-                    path="tweets"
-                    element={
-                        <AuthLayout authentication>
-                            <ChannelTweets />
-                        </AuthLayout>
-                    }
-                />
-                <Route
-                    path="subscribed"
-                    element={
-                        <AuthLayout authentication={false}>
-                            <ChannelSubscribers />
-                        </AuthLayout>
-                    }
-                />
+              <Route
+                path="videos"
+                element={
+                  <AuthLayout authentication>
+                    <ChannelVideos />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="playlists"
+                element={
+                  <AuthLayout authentication>
+                    <ChannelPlaylist />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="tweets"
+                element={
+                  <AuthLayout authentication>
+                    <ChannelTweets />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="subscribed"
+                element={
+                  <AuthLayout authentication={false}>
+                    <ChannelSubscribers />
+                  </AuthLayout>
+                }
+              />
             </Route>
 
             {/* Edit Channel Routes with Nested Personal Info and Password Routes */}
             <Route
-                path="edit"
-                element={
-                    <AuthLayout authentication>
-                        <EditChannel />
-                    </AuthLayout>
-                }
+              path="edit"
+              element={
+                <AuthLayout authentication>
+                  <EditChannel />
+                </AuthLayout>
+              }
             >
-                <Route
-                    path="personalInfo"
-                    element={
-                        <AuthLayout authentication>
-                            <EditPersonalInfo />
-                        </AuthLayout>
-                    }
-                />
-                <Route
-                    path="password"
-                    element={
-                        <AuthLayout authentication>
-                            <ChangePassword />
-                        </AuthLayout>
-                    }
-                />
+              <Route
+                path="personalInfo"
+                element={
+                  <AuthLayout authentication>
+                    <EditPersonalInfo />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="password"
+                element={
+                  <AuthLayout authentication>
+                    <ChangePassword />
+                  </AuthLayout>
+                }
+              />
             </Route>
-        </Route>
-    </Routes>
+          </Route>
+        </Routes>
+      </Suspense>
 
-    <Toaster
+      <Toaster
         position="top-right"
         reverseOrder={true}
         toastOptions={{
-            error: { style: { borderRadius: "0", color: "red" } },
-            success: { style: { borderRadius: "0", color: "green" } },
-            duration: 2000,
+          error: { style: { borderRadius: "0", color: "red" } },
+          success: { style: { borderRadius: "0", color: "green" } },
+          duration: 2000,
         }}
-    />
-</>
-  )
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
