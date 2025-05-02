1792be82e470bd17b9b02a72806945e7c38f85a9
Gunjankher <gkhercode@gmail.com>
2025-05-02 22:53:28 +0530

    adding loader in here

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+ import { useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
